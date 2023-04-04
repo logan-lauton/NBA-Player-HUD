37d938513582f331587fcb9b8e7e330ad466d591
Logan Lauton <Logan.lauton@marquette.edu>
2023-04-04 18:39:24 -0500

    Add files via upload

---
+++ iterations/app-v1.0/app.R
@@ -0,0 +1,143 @@
+##Logan Lauton
+##app.R - NBA Player HUD App v1
+##this is the app.R for the first iteration of my NBA Player HUD R Shiny app that I will
+##be producing. This file holds the main foundation of the apps appearance and brain.
+
+##clear your working directory--------------------------------------------------------------------------
+rm(list = ls())
+
+##load libraries----------------------------------------------------------------------------------------
+library(bslib)
+library(dplyr)
+library(DT)
+library(extrafont)
+library(ggridges)
+library(gridExtra)
+library(scales)
+library(shiny)
+library(shinythemes)
+library(tidyverse)
+
+##loading in data---------------------------------------------------------------------------------------
+ptot <- read.csv("~\\NBA Player Stats(1950 - 2022).csv")
+psal <- read.csv("~\\NBA Salaries(1990-2023).csv")
+tpay <- read.csv("~\\NBA Payroll(1990-2023).csv")
+
+#assign variable----------------------------------------------------------------------------------------
+latest_season <- 2022
+
+# Define UI for application-----------------------------------------------------------------------------
+ui <- navbarPage(
+  theme = shinytheme('flatly'),
+  id = "nba_player_hud",
+  imageOutput('kiwi'),
+  
+  #Main Tab-----------------------------------------------------------------------------------------------
+  tabPanel("Welcome",
+           fluidRow(column(
+             12,
+             wellPanel(style = "background-color: #fff00; border-color: #2c3e50;",
+                       includeMarkdown("~\\Test.md"))
+           ))),
+  
+  #Player Database----------------------------------------------------------------------------------------
+  tabPanel("NBA Player Database",
+           fluidRow(column(
+             7,
+             wellPanel(
+               style = "background-color: #fff; border-color: #1D4289; height: 775px;",
+               fluidRow(style = "margin-top: 25px; margin-bottom: -10px;",
+                        column(
+                          10,
+                          column(2, style = 'margin-top: 7px;', align = 'right', p("From:")),
+                          fluidRow(column(
+                            8,
+                            selectInput(
+                              inputId = 'banana',
+                              label = NULL,
+                              choices = sort(unique(ptot$Season), decreasing = T),
+                              selected = latest_season
+                            )
+                          )),
+                          DT::dataTableOutput("strawberry")
+                        ))
+             )
+           ))),
+  # Footer -------------------------------
+  hr(style = "border-color: #cbcbcb;"),
+  fluidRow(column(
+    9,
+    p(
+      'All of the data used to generate this app were obtained from ',
+      tags$a(
+        href = "https://www.basketball-reference.com/",
+        'basketball-reference.com',
+        target = '_blank'
+      ),
+      'and',
+      tags$a(href = "https://hoopshype.com/salaries/players/", 'hoopshype.com', target = '_blank'),
+      '.',
+      style = "font-size: 85%"
+    ),
+    p(
+      "App created by Logan Lauton in Spring of 2023",
+      HTML("&bull;"),
+      "Find Logan on Github:",
+      tags$a(
+        href = "https://github.com/logan-lauton",
+        tags$i(class = 'fa fa-github', style = 'color:#5000a5'),
+        target = '_blank'
+      ),
+      style = "font-size: 85%"
+    )
+  ),
+  column(
+    3,
+    align = "right",
+    conditionalPanel(
+      condition = "input.masters_golf == 'Scoring Averages' | input.masters_golf == 'Player Pages'",
+      p(
+        tags$em(
+          'Check the box below to change the colour scheme of the distributions to one that is more easily read by those with color blindness.',
+          style = "font-size: 70%; font-family:Helvetica"
+        )
+      ),
+      checkboxInput("col_blind", label = "Colourblind?", value = F)
+    )
+  )),
+  windowTitle = "NBA Player HUD"
+)
+
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  output$kiwi <- renderImage({
+    list(
+      src = "~//basketball-cover.png",
+      alt = "basketball",
+      height = 81,
+      width = 81,
+      style = "float:left; margin-left: 5px; margin-right: 5px; margin-top: -15px"
+    )
+  })
+  
+  
+  
+  output$strawberry <- renderDataTable({
+    datatable(
+      dplyr::filter(ptot, Season %in% c(input$banana)) %>%
+        select(Player, Pos, Age, Tm, BLK, AST, TRB, STL, FG. , FT.),
+      options = list(
+        dom = 't',
+        pageLength = 9,
+        paging = F,
+        scrollY = '550px'
+      )
+    )
+    
+  })
+  
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
---
+++ iterations/app-v1.0/global.R
@@ -0,0 +1,64 @@
+##Logan Lauton
+##global.R - NBA Player HUD App v1
+##this is the global.R for the first iteration of the NBA Player HUD R Shiny app that I will
+##be producing. In this holds my initial exploratory graphs and features that I would like to implement.
+
+##clear your working directory
+rm(list = ls())
+
+##load libraries
+library("ggplot2")
+library("dplyr")
+
+##loading in required data
+ptot <- read.csv("~\\NBA Player Stats(1950 - 2022).csv")
+psal <- read.csv("~\\NBA Salaries(1990-2023).csv")
+tpay <- read.csv("~\\NBA Payroll(1990-2023).csv")
+
+##important stats from empathy and define phases are
+##Blocks & assists, Rebounds & 3-pointers, Points , Steals , FG% and FT%
+
+##start by cleaning data before anything
+##begin by removing first two columns
+ptot <- ptot %>% select(-1, -2)
+##I will then remove all instances of TOT from the team(tm) column as this will largely scew data
+ptot <- subset(ptot, Tm != "TOT")
+
+##determine if column names are appropriate
+colnames(ptot)
+##colnames will suffice
+
+##next basic test of gathering all players in a full season, this will help for the end result
+dplyr::filter(ptot, Season %in% c(1950))
+
+
+##here I perform a basic group sumarise to output the number of field goal attempts per season by
+##the entire league
+ptot_season <- ptot %>% group_by(Season) %>%
+  summarise(FGA = sum(FGA),)
+##and finally I plot the data in a basic plot
+plot(ptot_season$Season, ptot_season$FGA)
+
+##now that I can see that the player data is successfully cleaned I will export it back to the same
+##name inorder to ensure no cleaning is needed in the app.R file and to not confuse .csv files
+write.csv(ptot, "~\\NBA Player Stats(1950 - 2022).csv", row.names = FALSE)
+
+##from here I can now begin cleaning the other datasets if necessary
+##removing first row of psal
+psal <- psal %>% select(-1)
+##check colnames
+colnames(psal)
+##colnames fine
+##export csv
+write.csv(psal, "~\\NBA Salaries(1990-2023).csv", row.names = FALSE)
+##removing first colimn of tpay
+tpay <- tpay %>% select(-1)
+##check colnames
+colnames(tpay)
+##colnames fine
+##export csv
+write.csv(psal, "~\\NBA Salaries(1990-2023).csv", row.names = FALSE)
+
+
+########################################################################################################
+##now I can begin exploring graphs and other features I'd like to implement
